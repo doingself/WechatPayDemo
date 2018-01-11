37c1a496896071f32eeebb485925ef9c54f1960a
syc <doingself@163.com>
2018-01-11 16:44:31 +0800

    small update ios

--- Swift_WechatPayDemo/WechatPayDemo/ViewController.swift
+++ Swift_WechatPayDemo/WechatPayDemo/ViewController.swift
@@ -9,28 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let btn = UIButton(frame: CGRect(x: 50, y: 100, width: 100, height: 100))
+        var y: CGFloat = 80
+        let pay = getBtn(title: "支付", frame: CGRect(x: 20, y: y, width: 100, height: 35))
+        pay.addTarget(self, action: #selector(self.payAction(sender:)), for: UIControlEvents.touchUpInside)
+        
+        y += 35 + 20
+        let query = getBtn(title: "查询", frame: CGRect(x: 20, y: y, width: 100, height: 35))
+        query.addTarget(self, action: #selector(self.queryAction(sender:)), for: UIControlEvents.touchUpInside)
+        
+        y += 35 + 20
+        let refund = getBtn(title: "退款", frame: CGRect(x: 20, y: y, width: 100, height: 35))
+        refund.addTarget(self, action: #selector(self.refundAction(sender:)), for: UIControlEvents.touchUpInside)
+        
+        
+        y += 35 + 20
+        textView = UITextView(frame: CGRect(x: 20, y: y, width: self.view.bounds.size.width-40, height: self.view.bounds.height-y-20))
+        textView.layer.borderWidth = 1
+        textView.delegate = self
+        self.view.addSubview(textView)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        textView.resignFirstResponder()
+    }
+    
+    // MARK: action
+    @objc func payAction(sender: Any?){
+        let path = "http://192.168.1.106:8080/pay?total=1"
+        sessionDataTaskRequestResume(path: path) { (dict: [String: Any]) in
+            //调起微信支付
+            let req = PayReq()
+            req.partnerId = dict["partnerid"] as! String
+            req.prepayId  = dict["prepayid"] as! String
+            req.nonceStr  = dict["noncestr"] as! String
+            req.timeStamp = dict["timestamp"] as! UInt32
+            req.package   = dict["package"] as! String
+            req.sign      = dict["sign"] as! String
+            DispatchQueue.main.async(execute: {
+                WXApi.send(req)
+            })
+        }
+    }
+    @objc func queryAction(sender: Any?){
+        
+    }
+    @objc func refundAction(sender: Any?){
+        
+    }
+    
+    // MARK: private func
+    private func getBtn(title: String, frame: CGRect) -> UIButton{
+        let btn = UIButton(frame: frame)
         btn.layer.borderWidth = 1
         btn.layer.masksToBounds = true
         btn.layer.cornerRadius = 10
-        btn.setTitle("wechat pay", for: UIControlState.normal)
+        btn.setTitle(title, for: UIControlState.normal)
         btn.setTitleColor(UIColor.black, for: UIControlState.normal)
         btn.setTitleColor(UIColor.lightGray, for: UIControlState.highlighted)
-        btn.addTarget(self, action: #selector(self.btnAction(sender:)), for: UIControlEvents.touchUpInside)
         self.view.addSubview(btn)
+        return btn
     }
-
-    @objc func btnAction(sender: Any?){
-        sessionget()
-    }
-    func sessionget(){
+    
+    private func sessionDataTaskRequestResume(path: String, completed: @escaping (_ dict: [String:Any])->Void){
         //1.创建请求路径
-        let path = "http://192.168.1.106:8080/pay?total=1"
+        //let path = "http://192.168.1.106:8080/pay?total=1"
         //拼接参数(GET请求参数需要以"?"连接拼接到请求地址的后面，多个参数用"&"隔开，参数形式：参数名=参数值)
         //转换成url(统一资源定位符)
         let url = URL(string: path)
@@ -62,119 +114,18 @@ class ViewController: UIViewController {
                 }
                 print(dict)
                 
-                //调起微信支付
-                let req = PayReq()
-                req.partnerId = dict["partnerid"] as! String
-                req.prepayId  = dict["prepayid"] as! String
-                req.nonceStr  = dict["noncestr"] as! String
-                req.timeStamp = dict["timestamp"] as! UInt32
-                req.package   = dict["package"] as! String
-                req.sign      = dict["sign"] as! String
-                DispatchQueue.main.async(execute: {
-                    WXApi.send(req)
-                })
+                completed(dict)
             }
             else{
                 print("请求数据失败")
+                self.textView.text = "请求失败 -> " + path
             }
         }
         //5.开始执行任务
         task.resume()
     }
-    
-    
-    
-    
-    
-    func sessionget2(){
-        //1.创建请求路径
-        let path = "http://192.168.1.106:8080/pay?total=1"
-        //拼接参数(GET请求参数需要以"?"连接拼接到请求地址的后面，多个参数用"&"隔开，参数形式：参数名=参数值)
-        //转换成url(统一资源定位符)
-        let url = URL(string: path)
-        
-        //2.创建请求对象
-        //NSURLRequest类型的请求对象的请求方式一定是GET(默认GET且不能被改变)
-        let request = URLRequest(url: url!)
-        
-        //3.根据会话模式创建session(创建默认会话模式)
-        //方式1：一般不采用
-        //let session = NSURLSession(configuration: NSURLSessionConfiguration.defaultSessionConfiguration())
-        //方式2：快速创建默认会话模式的session
-        let session = URLSession.shared
-        
-        //4.创建任务
-        //参数1：需要发送的请求对象
-        //参数2：服务返回数据的时候需要执行的对应的闭包
-        //闭包参数1：服务器返回给客户端的数据
-        //闭包参数2：服务器响应信息
-        //闭包参数3：错误信息
-        let task = session.dataTask(with: request) { (data: Data?, response: URLResponse!, err: Error?) in
-            
-            //注意：当前这个闭包是在子线程中执行的，如果想要在这儿执行UI操作必须通过线程间的通信回到主线程
-            
-            //            print(response)
-            //            print("能接受到")
-            //            print(NSThread.currentThread())
-            
-            //解析json
-            //参数options：.MutableContainers(json最外层是数组或者字典选这个选项)
-            if data != nil{
-                let dict = try! JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers)
-                print(dict)
-            }
-            else{
-                print("请求数据失败")
-            }
-        }
-        //5.开始执行任务
-        task.resume()
-    }
-    
-//    func sessionpost(){
-//        //1.创建请求地址
-//        let url = NSURL(string: "http://0.0.0.0:tuicool@api.tuicool.com/api/signup/register_by_email.json")
-//
-//        //2.创建求情对象
-//        //POST请求的请求对象必须使用NSMutableURLRequest创建，因为使用NSURLRequest不能更改请求方式，但是NSMutableURLRequest可以
-//        let request = NSMutableURLRequest(URL: url!)
-//        //设置请求方式(默认GET)
-//        request.HTTPMethod = "POST" //设置请求方式为POST请求
-//        //设置请求体(POST请求的参数是放到请求体中的)
-//        //a.拼接字符串
-//        //参数1=参数值1&参数2=参数2...
-//        let sendStr = "email=10165910@163.com&name=鱼摆摆&password=123456789"
-//        //b.将字符串转换成二进制
-//        //参数：编码方式
-//        let sendData = sendStr.dataUsingEncoding(NSUTF8StringEncoding)
-//        request.HTTPBody = sendData
-//
-//        //3.创建session
-//        //快速创建一个默认会话模式的session
-//        let session = NSURLSession.sharedSession()
-//
-//        //4.创建任务
-//        let task = session.dataTaskWithRequest(request) { (data, response, error) in
-//            print(response)
-//
-//            if data != nil{
-//                let ret = try! NSJSONSerialization.JSONObjectWithData(data!, options: .MutableContainers)
-//                print(ret)
-//            }
-//            else{
-//                print("请求失败")
-//            }
-//        }
-//
-//        //5.开始执行任务
-//        task.resume()
-//
-//    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+extension ViewController: UITextViewDelegate{
+    
+}
--- Swift_WechatPayDemo/WechatPayDemo/WechatPayDemo-Bridging-Header.h
+++ Swift_WechatPayDemo/WechatPayDemo/WechatPayDemo-Bridging-Header.h
@@ -2,5 +2,8 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+// wechat pay sdk 下载地址
+// https://open.weixin.qq.com/cgi-bin/showdocument?action=dir_list&t=resource/res_list&verify=1&id=open1419319164&lang=zh_CN
+
 #import "WechatAuthSDK.h"
 #import "WXApi.h"
